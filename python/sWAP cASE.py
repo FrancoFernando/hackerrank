e3a39cf6e9458c29d4b35c8344c559fe6f97d12e
Franco Fernando <69932537+FrancoFernando@users.noreply.github.com>
2022-04-04 22:46:05 +0200

    Create sWAP cASE.py

---
+++ python/sWAP cASE.py
@@ -0,0 +1,8 @@
+def swap_case(s):
+    return "".join(c.swapcase() for c in s)
+    #return "".join(c.upper() if c.islower() else c.lower() for c in s)
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
