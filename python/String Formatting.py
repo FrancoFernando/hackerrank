ed93d5906b9178daa1227b36f9f407c5dfe4a459
Franco Fernando <69932537+FrancoFernando@users.noreply.github.com>
2022-04-12 15:59:32 +0200

    Create String Formatting.py

---
+++ python/String Formatting.py
@@ -0,0 +1,14 @@
+def print_formatted(number):
+    width = len(f"{number:b}")
+    for num in range(1,number+1):
+        binary = bin(num)[2:].rjust(width,' ')
+        octal = oct(num)[2:].rjust(width,' ')
+        hexadecimal = hex(num)[2:].upper().rjust(width,' ')
+        decimal = str(num).rjust(width,' ')
+        print(decimal,octal,hexadecimal,binary)
+            
+        
+    
+if __name__ == '__main__':
+    n = int(input())
+    print_formatted(n)
