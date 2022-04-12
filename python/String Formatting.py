410e7c1b57dbf4539f819e550e9d6568d140a7bb
Franco Fernando <69932537+FrancoFernando@users.noreply.github.com>
2022-04-12 16:13:40 +0200

    Update String Formatting.py

--- python/String Formatting.py
+++ python/String Formatting.py
@@ -1,3 +1,13 @@
+# with fstrings
+def print_formatted(number):
+    width = len(f"{number:b}")
+    for num in range(1,number+1):
+        binary = f"{num:b}".rjust(width,' ')
+        octal = f"{num:o}".rjust(width,' ')
+        hexadecimal = f"{num:X}".rjust(width,' ')
+        decimal = str(num).rjust(width,' ')
+        print(decimal,octal,hexadecimal,binary)
+'''
 def print_formatted(number):
     width = len(f"{number:b}")
     for num in range(1,number+1):
@@ -6,7 +16,7 @@ def print_formatted(number):
         hexadecimal = hex(num)[2:].upper().rjust(width,' ')
         decimal = str(num).rjust(width,' ')
         print(decimal,octal,hexadecimal,binary)
-            
+ '''       
         
     
 if __name__ == '__main__':
