ccdf3e8d4d8b3e91d37714f2f05a96a4b25429f4
Franco Fernando <69932537+FrancoFernando@users.noreply.github.com>
2022-04-13 10:58:06 +0200

    Create Alphabet Rangoli.py

---
+++ python/Alphabet Rangoli.py
@@ -0,0 +1,20 @@
+from string import ascii_lowercase
+
+def print_rangoli(size):
+    
+    letters = ascii_lowercase[:size]
+    rangoli = []
+    row_size = 4*size-3
+    
+    for i in range(size):
+        suffix = '-'.join(letters[size-i:])
+        prefix = suffix[::-1]
+        letter = letters[size-i-1]
+        rangoli.append('-'.join([prefix,letter,suffix]).center(row_size,'-'))
+    
+    rangoli.extend(rangoli[-2::-1])
+    print('\n'.join(rangoli))
+
+if __name__ == '__main__':
+    n = int(input())
+    print_rangoli(n)
